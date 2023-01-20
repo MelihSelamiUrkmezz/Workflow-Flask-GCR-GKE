a61d3db5831188fc51739dba4580c8d8071719b1
MelihSelamiUrkmez <melih.s.urkmez@gmail.com>
2023-01-20 12:16:43 +0300

    Add good bye path

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+@app.route("/goodbye")
+def good_bye():
+    return "Good bye!"
+
 @app.route("/hello")
 def hello_world():
     return "Hello, World!"
